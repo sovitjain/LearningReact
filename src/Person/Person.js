578e7c0761edc94fd90899e2afda3d86536d8a2b
Sovit Jain <sovit.jain@instarem.com>
2020-06-29 22:17:41 +0530

    added 2 files

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const person =  (props) =>  {
    return (
-       <div>
+       <div className="Person">
             <h1 onClick={props.click}> My name is {props.name}, {props.age}</h1>
             {props.children}
             <input type="text" onChange={props.click1}/>
